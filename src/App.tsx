8c3661effcf6dd3ae9a45a951de754454d5ccca3
Elliot <est@udel.edu>
2021-09-29 16:22:58 -0400

    leaving it alone for now. dropdowns not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { NavDropdown } from 'react-bootstrap';
 
 import './App.css';
 
@@ -65,7 +65,15 @@ function App() {
       <div>
         <button className="button-action" onClick={insertRandomColor}>Randomize</button>
 
-        <button className="button-dropdown">Custom</button>
+        <div className="dropdown">
+          <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
+            <span className="caret">Custom</span></button>
+              <ul className="dropdown-menu">
+                <li>Bruh1</li>
+                <li>Bruh2</li>
+              </ul>
+        </div>
+        
 
         <button className="button-action" onClick={resetColor}>Reset</button>
         <button className="button-dropdown">History</button>
