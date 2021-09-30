9347a6aa13353151801843ee06ab604d7ad4f4f3
Elliot <est@udel.edu>
2021-09-29 20:23:49 -0400

    more framework for diff color types

--- src/App.tsx
+++ src/App.tsx
@@ -55,20 +55,24 @@ function RBGTextChange() {
 }
 
 
-
-var siteColor = '123456';
+var colorType = 'RGB';
+var hexSiteColor = '123456';
+var RGBred = '18';
+var RGBgreen = '52';
+var RGBblue = '86';
 //TODO add correct color appearing and changing
+//TODO if RGB mode use, if hex mode use, etc for h2 and other relevant parts
+//TODO customize dropdown buttons
 function App() {
   return (
     <div className="App">
       <header className="App-header" id="headerID">
-      <h1 className="h1title"> Background Color Selector</h1>
 
-      
-      <h2>Your Color: #{siteColor}</h2>
+      <h1 className="h1title"> Background Color Selector</h1>
+      <h2 className="h2title">Color Formatting Selected: {colorType}</h2>
+      <h2 className="h2title2">Your Colors:  Red:{RGBred}  Green:{RGBgreen}  Blue:{RGBblue}</h2>
 
       </header>
-
 
 
       <body className="App-body" id="bodyID">
@@ -82,18 +86,23 @@ function App() {
           <div className="dropdown">
             <button className="dropbtn">Custom</button>
             <div className="dropdown-content" style={{right:10}}>
-              <button className="button-action" onClick={insertRandomColor}>Randomize</button>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
+              <div><button className="custom-drop-btn">rgb</button></div>
+              <div><button className="custom-drop-btn">Hex Values</button></div>
+              <div><button className="custom-drop-btn">hsla</button></div>
+              <div><button className="custom-drop-btn">RBG</button></div>
+              <div><a href="#">Link 3</a></div>
             </div>
           </div>
+
 
           <div className="dropdown">
             <button className="dropbtn">History</button>
             <div className="dropdown-content" style={{left:10}}>
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
+              <div><button className="custom-drop-btn">rgb</button></div>
+              <div><button className="custom-drop-btn">Hex Values</button></div>
+              <div><button className="custom-drop-btn">hsla</button></div>
+              <div><button className="custom-drop-btn">RBG</button></div>
+              <div><a href="#">Link 3</a></div>
             </div>
           </div>
 
